54953ad03bf622e3e1cdcc2f13b0c56ebc29dd13
Danielek98 <danielek2606@gmail.com>
2016-04-24 19:47:50 +0200

    FileList control applied

--- Commander/ViewModels/MainWindowViewModel.cs
+++ Commander/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.Practices.Prism.Mvvm;
+﻿using System.IO;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Mvvm;
+using Microsoft.Practices.Prism.Commands;
 
 namespace Commander.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        
+        public MainWindowViewModel()
+        {
+            FileClicked = new DelegateCommand<FileSystemInfo>(ExecuteMethod);
+        }
+
+        private void ExecuteMethod(FileSystemInfo o)
+        {
+            o.ToString();
+        }
+
+        public ICommand FileClicked { get; private set; }
     }
 }
