cce97cc51db8e72b4157cc9a17cff44f702317b4
Daniel Sroka <d.sroka@ela.pl>
2016-05-02 20:59:07 +0200

    sorting files

--- Commander/Controls/FileList/ViewModels/FileListViewModel.cs
+++ Commander/Controls/FileList/ViewModels/FileListViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
@@ -17,15 +20,14 @@ namespace Commander.Controls.FileList.ViewModels
         private ObservableCollection<FileSystemItemViewModel> _files =
             new ObservableCollection<FileSystemItemViewModel>();
 
+        private CollectionViewSource _filesDataView = new CollectionViewSource();
+        private string _sortColumn;
+        private ListSortDirection _sortDirection;
+
         public FileListViewModel()
         {
             LoadPathCommand = new DelegateCommand<string>(GetPathFiles);
-            OrderCommand = new DelegateCommand<string>(ExecuteMethod);
-        }
-
-        private void ExecuteMethod(string s)
-        {
-            throw new NotImplementedException();
+            OrderCommand = new DelegateCommand<string>(OrderFiles);
         }
 
         public string CurrentPath
@@ -37,35 +39,59 @@ namespace Commander.Controls.FileList.ViewModels
         public ObservableCollection<FileSystemItemViewModel> Files
         {
             get { return _files; }
-            set { SetProperty(ref _files, value); }
+            set
+            {
+                SetProperty(ref _files, value);
+                _filesDataView = new CollectionViewSource {Source = value};
+                OnPropertyChanged(nameof(FilesDataView));
+            }
         }
 
         public ICommand LoadPathCommand { get; private set; }
         public ICommand OrderCommand { get; private set; }
+        public ListCollectionView FilesDataView => _filesDataView.View as ListCollectionView;
 
         private void GetPathFiles(string path)
         {
             Task.Run(() =>
             {
                 var rootDirectory = new DirectoryInfo(path);
-                Files =
-                    new ObservableCollection<FileSystemItemViewModel>(
-                        rootDirectory.EnumerateFileSystemInfos()
-                            .Where(f => (f.Attributes & (FileAttributes.System | FileAttributes.Hidden)) == 0)
-                            .Select(f => FileSystemItemViewModel.Create(f.FullName)));
-
+                UiInvoke(() =>
+                    Files =
+                        new ObservableCollection<FileSystemItemViewModel>(
+                            rootDirectory.EnumerateFileSystemInfos()
+                                .Where(f => (f.Attributes & (FileAttributes.System | FileAttributes.Hidden)) == 0)
+                                .Select(f => FileSystemItemViewModel.Create(f.FullName)))
+                    );
                 if (rootDirectory.Parent != null)
-                    Files.Insert(0, new DirectoryViewModel(rootDirectory.Parent?.FullName) {DisplayName = ".."});
+                    UiInvoke(() => Files.Insert(0, new DirectoryViewModel(rootDirectory.Parent?.FullName) {DisplayName = ".."}));
                 CurrentPath = path;
             });
         }
 
-        //private static void UiInvoke(Action action)
-        //{
-        //    if (!Application.Current.Dispatcher.CheckAccess())
-        //        Application.Current.Dispatcher.Invoke(action);
-        //    else
-        //        action();
-        //}
+        private void OrderFiles(string column)
+        {
+            if (_sortColumn != column)
+            {
+                _sortColumn = column;
+                _sortDirection = ListSortDirection.Ascending;
+            }
+            else
+                _sortDirection = _sortDirection == ListSortDirection.Ascending
+                    ? ListSortDirection.Descending
+                    : ListSortDirection.Ascending;
+
+            _filesDataView.SortDescriptions.Clear();
+            _filesDataView.SortDescriptions.Add(
+                new SortDescription(_sortColumn, _sortDirection));
+        }
+
+        private static void UiInvoke(Action action)
+        {
+            if (!Application.Current.Dispatcher.CheckAccess())
+                Application.Current.Dispatcher.Invoke(action);
+            else
+                action();
+        }
     }
 }
